2acaffbd5cc512c05c21883e1abb33f23d3979c9
aichaib1228-beep <aichaib1228@gmail.com>
2025-12-09 22:33:24 +0100

    Initialize next.config.js with basic configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Eventuelt kan du legge til konfigurasjon for eksterne bilder her:
+  // images: {
+  //   domains: ['example.com'],
+  // },
+};
+
+module.exports = nextConfig;
